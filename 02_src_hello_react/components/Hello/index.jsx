6ee0795bd58cf87d5cb75da6bf28662ec770a4dd
wangnan <pumpkin_n@qq.com>
2025-07-04 14:42:08 +0800

    react学习

---
+++ 02_src_hello_react/components/Hello/index.jsx
@@ -0,0 +1,7 @@
+import React ,{Component} from "react";
+import './index.css'
+export default class Hello extends Component{
+    render(){
+        return <div className='helloCss'>Hello</div>
+    }
+}
