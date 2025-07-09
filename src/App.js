dba019ef9bf11dc7c8fe5ec89a9722aaaa29c3e0
wangnan <pumpkin_n@qq.com>
2025-07-09 15:58:20 +0800

    axios

--- src/App.js
+++ src/App.js
@@ -1,69 +1,21 @@
 import React, { Component } from "react";
-import Header from "./components/Header";
-import List from "./components/List";
-import Footer from "./components/Footer";
+import axios from "axios";
 
-import "./App.css";
 export default class App extends Component {
-  state = {
-    todoList: [
-      { id: "001", name: "吃饭", done: true },
-      { id: "002", name: "睡觉", done: true },
-      { id: "003", name: "喝奶茶", done: false },
-    ],
-  };
-  addTodo = (data) => {
-    const { todoList } = this.state;
-    const newTodo = [data, ...todoList];
-    console.log(newTodo);
-    this.setState({ todoList: newTodo });
-  };
-  updateTodo = (id, done) => {
-    const { todoList } = this.state;
-    const newTodos = todoList.map((todoObj) => {
-      if (todoObj.id === id) return { ...todoObj, done };
-      else return { ...todoObj };
-    });
-    this.setState({ todoList: newTodos });
-  };
-  deleteTodo = (id) => {
-    const { todoList } = this.state;
-    const newTodos = todoList.filter((todoObj) => {
-      return todoObj.id !== id;
-    });
-    this.setState({ todoList: newTodos });
-  };
-  checkAllTodo = (done) => {
-    const { todoList } = this.state;
-    const newTodos = todoList.map((todoObj) => {
-      return { ...todoObj, done };
-    });
-    this.setState({ todoList: newTodos });
-  };
-  clearAllTodo = () => {
-    const { todoList } = this.state;
-    const newTodos = todoList.map((todoObj) => {
-      return { ...todoObj, done: false };
-    });
-    this.setState({ todoList: newTodos });
+  getStudentData = () => {
+    axios.get("http://localhost:3000/students").then(
+      (response) => {
+        console.log("成功了", response.data);
+      },
+      (error) => {
+        console.log("失败了", error);
+      }
+    );
   };
   render() {
-    const { todoList } = this.state;
     return (
-      <div className="todo-container">
-        <div className="todo-wrap">
-          <Header addTodo={this.addTodo} />
-          <List
-            todos={todoList}
-            updateTodo={this.updateTodo}
-            deleteTodo={this.deleteTodo}
-          />
-          <Footer
-            todos={todoList}
-            checkAllTodo={this.checkAllTodo}
-            clearAllTodo={this.clearAllTodo}
-          />
-        </div>
+      <div>
+        <button onClick={this.getStudentData}>点我获取学生数据</button>
       </div>
     );
   }
